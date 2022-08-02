dc168656115ae4171c0eebe29150ab65b0aebe6c
= <=>
2022-08-02 22:12:35 +0530

    insert and inorder binary search tree

--- trees.cpp
+++ trees.cpp
@@ -1,19 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
     struct Node *left;
     struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node * insert(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        return new Node(val);
+    }
+
+    if (val < root->data)
+    {
+        root->left = insert(root->left, val);
+    }
+    else if (val > root->data)
+    {
+        root->right = insert(root->right, val);
+    }
+
+    return root;
 }
 
-struct tree{
-    unsigned int height = 0;
-    struct Node *root = NULL;
+void inOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    else
+    {
+        inOrder(root->left);
+        cout << root->data << "->";
+        inOrder(root->right);
+        return;
+    }
 }
+int main()
+{
+    Node *root = NULL;
+    int arr[] = { 5, 1, 3, 4, 2, 7 };
+    root = insert(root, arr[0]);
+    for (int i = 1; i < 6; i++)
+    {
+        insert(root, arr[i]);
+    }
 
-
-int main(){
-    
+    inOrder(root);
+    return 0;
 }
