41deecb428a9dd8c83ad37d69bd2cbba5ac6bfdf
= <=>
2022-08-29 20:49:53 +0530

    reverse String using recursion one varibale

--- Recursion/reverseString.cpp
+++ Recursion/reverseString.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 
-void ReverseString(string& str,int start,int end){
-    if(start >= end){
+void ReverseString(string& str,int start, int size){
+    if(start >= size/2){
         return;
     }
-    swap(str[start],str[end]);
-    ReverseString(str,start+1,end-1);
+    swap(str[start],str[size-start-1]);
+    ReverseString(str,start+1,size);
 
 }
 int main()
 {
     string name = "pranay";
     cout <<"Before Reverse: "<<name<<endl;
-    ReverseString(name,0,5);
+    ReverseString(name,0,name.length());
 
     cout <<"After Reverse: "<<name;
     return 0;
